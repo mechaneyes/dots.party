6904566e8352c23bca6e01d3d707d0802985ceea
Ray Weitzenberg <ray@mechaneyes.com>
2022-08-31 04:22:07 -0700

    Socket is initializing + Socket is already running
    + first bit of socket.io setup

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,21 @@
-const About = () => {
-  return <div>About</div>;
-};
+import { useEffect } from 'react'
+import io from 'Socket.IO-client'
 
-export default About;
+let socket
+
+const Eyesee = () => {
+  useEffect(() => socketInitializer(), [])
+
+  const socketInitializer = async () => {
+    await fetch('/api/socket')
+    socket = io()
+
+    socket.on('connect', () => {
+      console.log('connected')
+    })
+  }
+
+  return null
+}
+
+export default Eyesee;
---
+++ pages/api/socket.js
@@ -0,0 +1,18 @@
+// Implementing WebSocket communication in Next.js on LogRocket
+// 
+// https://blog.logrocket.com/implementing-websocket-communication-next-js/
+
+import { Server } from "Socket.IO";
+
+const SocketHandler = (req, res) => {
+  if (res.socket.server.io) {
+    console.log("Socket is already running");
+  } else {
+    console.log("Socket is initializing");
+    const io = new Server(res.socket.server);
+    res.socket.server.io = io;
+  }
+  res.end();
+};
+
+export default SocketHandler;

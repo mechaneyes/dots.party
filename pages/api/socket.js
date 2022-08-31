520298d733989848a11b6e06fb9b840428d1a9f6
Ray Weitzenberg <ray@mechaneyes.com>
2022-08-31 04:53:47 -0700

    logRocket example working

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,18 +1,23 @@
 // Implementing WebSocket communication in Next.js on LogRocket
-// 
 // https://blog.logrocket.com/implementing-websocket-communication-next-js/
 
-import { Server } from "Socket.IO";
+import { Server } from 'Socket.IO'
 
 const SocketHandler = (req, res) => {
   if (res.socket.server.io) {
-    console.log("Socket is already running");
+    console.log('Socket is already running')
   } else {
-    console.log("Socket is initializing");
-    const io = new Server(res.socket.server);
-    res.socket.server.io = io;
-  }
-  res.end();
-};
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
 
-export default SocketHandler;
+    io.on('connection', socket => {
+      socket.on('input-change', msg => {
+        socket.broadcast.emit('update-input', msg)
+      })
+    })
+  }
+  res.end()
+}
+
+export default SocketHandler
--- pages/eyesee.js
+++ pages/eyesee.js
@@ -1,21 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from 'react'
 import io from 'socket.io-client'
-
 let socket;
 
 const Eyesee = () => {
-  useEffect(() => socketInitializer(), []);
+  const [input, setInput] = useState('')
+
+  useEffect(() => socketInitializer(), [])
 
   const socketInitializer = async () => {
-    await fetch("/api/socket");
-    socket = io();
+    await fetch('/api/socket');
+    socket = io()
 
-    socket.on("connect", () => {
-      console.log("connected");
-    });
-  };
+    socket.on('connect', () => {
+      console.log('connected')
+    })
 
-  return null;
-};
+    socket.on('update-input', msg => {
+      setInput(msg)
+    })
+  }
+
+  const onChangeHandler = (e) => {
+    setInput(e.target.value)
+    socket.emit('input-change', e.target.value)
+  }
+
+  return (
+    <input
+      placeholder="Type something"
+      value={input}
+      onChange={onChangeHandler}
+    />
+  )
+}
 
 export default Eyesee;

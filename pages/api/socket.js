90087f8774e5b1338ba2c680d6d7c58281aec430
Ray Weitzenberg <ray@mechaneyes.com>
2022-08-31 05:36:00 -0700

    rudimentary p5 implementation

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -8,6 +8,7 @@ const SocketHandler = (req, res) => {
     console.log('Socket is already running')
   } else {
     console.log('Socket is initializing')
+    
     const io = new Server(res.socket.server)
     res.socket.server.io = io
 
--- pages/eyesee.js
+++ pages/eyesee.js
@@ -1,37 +1,51 @@
-import { useEffect, useState } from 'react'
-import io from 'socket.io-client'
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+// The syntax for named exports is slightly different.
+// You can use the widget with a dynamic import as follows:
+// https://stackoverflow.com/a/65735539
+// 
+import dynamic from "next/dynamic";
+const P5Rect = dynamic(
+  () => import("../components/p5-rect").then((mod) => mod.P5Rect),
+  { ssr: false }
+);
+
 let socket;
 
 const Eyesee = () => {
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState("");
 
-  useEffect(() => socketInitializer(), [])
+  useEffect(() => socketInitializer(), []);
 
   const socketInitializer = async () => {
-    await fetch('/api/socket');
-    socket = io()
+    await fetch("/api/socket");
+    socket = io();
 
-    socket.on('connect', () => {
-      console.log('connected')
-    })
+    socket.on("connect", () => {
+      console.log("connected");
+    });
 
-    socket.on('update-input', msg => {
-      setInput(msg)
-    })
-  }
+    socket.on("update-input", (msg) => {
+      setInput(msg);
+    });
+  };
 
   const onChangeHandler = (e) => {
-    setInput(e.target.value)
-    socket.emit('input-change', e.target.value)
-  }
+    setInput(e.target.value);
+    socket.emit("input-change", e.target.value);
+  };
 
   return (
-    <input
-      placeholder="Type something"
-      value={input}
-      onChange={onChangeHandler}
-    />
-  )
-}
+    <>
+      <input
+        placeholder="Type something"
+        value={input}
+        onChange={onChangeHandler}
+      />
+      <P5Rect />
+    </>
+  );
+};
 
 export default Eyesee;

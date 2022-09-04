8c00e8caa28889258fbaad9679136e8e88901a12
Ray Weitzenberg <ray@mechaneyes.com>
2022-09-04 04:43:03 -0700

    Invalid Usage of suspense Option of next/dynamic

--- pages/dots.js
+++ pages/dots.js
@@ -1,17 +1,11 @@
 import dynamic from "next/dynamic";
-import { Suspense } from "react";
 
 const DotOne = dynamic(() => import("../components/DotOne"), {
-  suspense: true,
   ssr: false,
 });
 
 const Dots = () => {
-  return (
-    <Suspense fallback={`Loading...`}>
-      <DotOne />
-    </Suspense>
-  );
+  return <DotOne />;
 };
 
 export default Dots;

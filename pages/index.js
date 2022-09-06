b4d9a04d6858a47628e22f3ecc7f7c8e18819fb5
Ray Weitzenberg <ray@mechaneyes.com>
2022-09-05 18:29:27 -0700

    housekeeping

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const DotOne = dynamic(() => import("../components/DotOne"), { ssr: false });
 export default function Home() {
   return (
     <div className="app">
-      return <DotOne colorway="colStadiumCar" />;
+      <DotOne colorway="colStadiumCar" />;
     </div>
   )
 }

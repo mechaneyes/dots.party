e2c7c2215729e2b842634b38cd2b386a52b3e031
Ray Weitzenberg <ray@mechaneyes.com>
2022-09-08 03:42:09 -0700

    emote tweak

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,19 @@ import { useState } from "react";
 const DotOne = dynamic(() => import("../components/DotOne"), { ssr: false });
 
 export default function Home() {
-  const [colorway, setColorway] = useState('colUtopia');
+  const [colorway, setColorway] = useState("colUtopia");
 
   return (
     <div className="app">
       <div className="navbar">
-        <button onClick={() => setColorway('colColdGarden')}>
+        <button onClick={() => setColorway("colColdGarden")}>
           <h3>🍖</h3>
         </button>
-        <button onClick={() => setColorway('colStadiumCar')}>
-          <h3>🏂</h3>
+        <button onClick={() => setColorway("colStadiumCar")}>
+          <h3>🪬</h3>
         </button>
-        <button onClick={() => setColorway('colUtopia')}>
-          <h3>🔥</h3>
+        <button onClick={() => setColorway("colUtopia")}>
+          <h3>🏂</h3>
         </button>
       </div>
       <DotOne colorway={colorway} />;

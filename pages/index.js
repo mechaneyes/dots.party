ce14cdc80519098dea3f255b03ca4a0542bfcac9
Ray Weitzenberg <ray@mechaneyes.com>
2022-11-28 22:13:20 -0800

    metadata w temp og:image

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Script from "next/script";
 import { useState } from "react";
 import dynamic from "next/dynamic";
+import Head from "next/head";
+
 const DotOne = dynamic(() => import("../components/DotOne"), { ssr: false });
 
 export default function Home() {
@@ -12,23 +14,37 @@ export default function Home() {
   };
 
   return (
-    <div className="app">
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-V4X33FRRRS"
-        strategy="afterInteractive"
-      ></Script>
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
+    <>
+      <Head>
+        <title>Dots Dot Party</title>
+        <meta property="og:title" content="Dots Dot Party" />
+        <meta
+          property="og:url"
+          content="https://dots.party/"
+        />
+        <meta
+          property="og:image"
+          content="https://i.pinimg.com/originals/53/bf/0c/53bf0cb9dc37ae1e223dac5eb1c0519a.jpg"
+        />
+        <meta name="description" content="Hoo Gots the Dots?" />
+      </Head>
+      <div className="app">
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-V4X33FRRRS"
+          strategy="afterInteractive"
+        ></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'G-V4X33FRRRS');
       `}
-      </Script>
+        </Script>
 
-      {/* <div className="navbar">
+        {/* <div className="navbar">
         <button onClick={() => colorHandler('colorwayGarden')}>
           <h3>🍖</h3>
         </button>
@@ -39,10 +55,11 @@ export default function Home() {
           <h3>🏂</h3>
         </button>
       </div> */}
-      <p className="feedback">
-        <a href="mailto:ray@mechaneyes.com">feedback</a>
-      </p>
-      <DotOne colorway={colorway} />
-    </div>
+        <p className="feedback">
+          <a href="mailto:ray@mechaneyes.com">feedback</a>
+        </p>
+        <DotOne colorway={colorway} />
+      </div>
+    </>
   );
 }

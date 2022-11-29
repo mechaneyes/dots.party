041b656de9ef8255e169cccd6fbcdb9c009b62ce
Ray Weitzenberg <ray@mechaneyes.com>
2022-11-28 22:26:36 -0800

    metta + adding images to /public

--- pages/index.js
+++ pages/index.js
@@ -17,16 +17,15 @@ export default function Home() {
     <>
       <Head>
         <title>Dots Dot Party</title>
+        <meta name="description" content="Hoo Gots the Dots?" />
+        <meta property="og:url" content="https://dots.party/" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="Dots Dot Party" />
-        <meta
-          property="og:url"
-          content="https://dots.party/"
-        />
+        <meta property="og:description" content="Hoo Gots the Dots?" />
         <meta
           property="og:image"
           content="https://i.pinimg.com/originals/53/bf/0c/53bf0cb9dc37ae1e223dac5eb1c0519a.jpg"
         />
-        <meta name="description" content="Hoo Gots the Dots?" />
       </Head>
       <div className="app">
         <Script

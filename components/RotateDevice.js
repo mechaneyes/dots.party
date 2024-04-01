b2a22bb32ba3bd121dc08f993bb618898d260513
mechaneyes <ray@mechaneyes.com>
2024-04-01 11:19:54 -0400

    updated rotate screen
    ° added icon
    ° added animation to icon
    ° swapped in better bg color

--- components/RotateDevice.js
+++ components/RotateDevice.js
@@ -1,24 +1,27 @@
 const RotateDevice = () => {
   if (typeof window !== "undefined") {
     window.addEventListener("resize", function () {
-      const rotateDeviceElement = document.querySelector(".rotate-device");
+      const rotateDeviceDiv = document.querySelector(".rotate");
 
       if (window.innerHeight > window.innerWidth) {
         // Screen orientation is vertical
-        rotateDeviceElement.style.display = "flex";
+        rotateDeviceDiv.style.display = "flex";
       } else {
         // Screen orientation is horizontal
-        rotateDeviceElement.style.display = "none";
+        rotateDeviceDiv.style.display = "none";
       }
     });
 
     // Call the event handler once to set the initial state
     window.dispatchEvent(new Event("resize"));
   }
-  
+
   return (
     <>
-      <p className="rotate-device">rotate your device</p>
+      <div className="rotate">
+        <p className="rotate__copy">rotate your device</p>
+        <img className="rotate__image" src="/images/rotate.gif" alt="rotate device" />
+      </div>
     </>
   );
 };
